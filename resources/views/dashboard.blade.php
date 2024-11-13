169b59f034507860b27a06eb98e2ad296653d603
hamzalenumero <140252655+hamzalenumero@users.noreply.github.com>
2024-11-13 21:03:08 +0100

    icon j'aime

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,7 +19,14 @@
                             </div>
                         @endif
                         <div class="p-4 bg-gray-800">
-                            <h3 class="text-white text-lg font-semibold mb-2 truncate">{{ $movie['title'] }}</h3>
+                            <div class="flex justify-between items-center mb-2">
+                                <h3 class="text-white text-lg font-semibold truncate">{{ $movie['title'] }}</h3>
+                                <button class="text-gray-400 hover:text-red-500 transition-colors duration-200">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                    </svg>
+                                </button>
+                            </div>
                             <div class="flex justify-between items-center">
                                 <span class="text-purple-500 font-bold">{{ number_format($movie['vote_average'], 1) }}/10</span>
                                 <span class="text-gray-400">{{ \Carbon\Carbon::parse($movie['release_date'])->format('Y') }}</span>
@@ -50,7 +57,14 @@
                             </div>
                         @endif
                         <div class="p-4 bg-gray-800">
-                            <h3 class="text-white text-lg font-semibold mb-2 truncate">{{ $movie['title'] }}</h3>
+                            <div class="flex justify-between items-center mb-2">
+                                <h3 class="text-white text-lg font-semibold truncate">{{ $movie['title'] }}</h3>
+                                <button class="text-gray-400 hover:text-red-500 transition-colors duration-200">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                    </svg>
+                                </button>
+                            </div>
                             <div class="flex justify-between items-center">
                                 <span class="text-purple-500 font-bold">{{ number_format($movie['vote_average'], 1) }}/10</span>
                                 <span class="text-gray-400">{{ \Carbon\Carbon::parse($movie['release_date'])->format('Y') }}</span>
--- resources/views/movies/movie.blade.php
+++ resources/views/movies/movie.blade.php
@@ -16,7 +16,14 @@
                         @endif
                         
                         <div class="p-4">
-                            <h2 class="text-xl font-bold text-white mb-2">{{ $movie['title'] }}</h2>
+                            <div class="flex justify-between items-center mb-2">
+                                <h2 class="text-xl font-bold text-white">{{ $movie['title'] }}</h2>
+                                <button class="text-gray-400 hover:text-red-500 transition-colors duration-200">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                    </svg>
+                                </button>
+                            </div>
                             <div class="flex justify-between items-center">
                                 <span class="text-purple-500 font-bold">{{ number_format($movie['vote_average'], 1) }}/10</span>
                                 <span class="text-gray-400">{{ \Carbon\Carbon::parse($movie['release_date'])->format('Y') }}</span>
@@ -40,7 +47,14 @@
                         @endif
                         
                         <div class="p-4">
-                            <h2 class="text-xl font-bold text-white mb-2">{{ $tvShow['name'] }}</h2>
+                            <div class="flex justify-between items-center mb-2">
+                                <h2 class="text-xl font-bold text-white">{{ $tvShow['name'] }}</h2>
+                                <button class="text-gray-400 hover:text-red-500 transition-colors duration-200">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                    </svg>
+                                </button>
+                            </div>
                             <div class="flex justify-between items-center">
                                 <span class="text-purple-500 font-bold">{{ number_format($tvShow['vote_average'], 1) }}/10</span>
                                 <span class="text-gray-400">{{ \Carbon\Carbon::parse($tvShow['first_air_date'])->format('Y') }}</span>
