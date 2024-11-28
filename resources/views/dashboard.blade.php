dc29f6fbd7b9e6fe2f1e17187859a83af5c85e57
hamzalenumero <140252655+hamzalenumero@users.noreply.github.com>
2024-11-28 15:27:01 +0100

    favoris

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -36,39 +36,7 @@
                                 <h2 class="text-xl font-bold text-white hover:text-purple-500">
                                     {{ $movie['title'] }}
                                 </h2>
-                                @auth
-                                    @php
-                                        $isFavorite = Auth::user()->favorites()
-                                            ->where('tmdb_id', $movie['id'])
-                                            ->where('type', 'movie')
-                                            ->exists();
-                                    @endphp
-
-                                    @if($isFavorite)
-                                        <form action="{{ route('favorites.destroy') }}" method="POST" class="inline">
-                                            @csrf
-                                            @method('DELETE')
-                                            <input type="hidden" name="tmdb_id" value="{{ $movie['id'] }}">
-                                            <input type="hidden" name="type" value="movie">
-                                            <button type="submit" class="text-purple-500 hover:text-purple-700 transition-colors duration-200">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                                                </svg>
-                                            </button>
-                                        </form>
-                                    @else
-                                        <form action="{{ route('favorites.store') }}" method="POST" class="inline">
-                                            @csrf
-                                            <input type="hidden" name="tmdb_id" value="{{ $movie['id'] }}">
-                                            <input type="hidden" name="type" value="movie">
-                                            <button type="submit" class="text-gray-400 hover:text-purple-500 transition-colors duration-200">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                                                </svg>
-                                            </button>
-                                        </form>
-                                    @endif
-                                @endauth
+                                <x-favorite-button :id="$movie['id']" type="movie" />
                             </div>
                             <div class="flex justify-between items-center mt-2">
                                 <span class="text-purple-500 font-bold">
